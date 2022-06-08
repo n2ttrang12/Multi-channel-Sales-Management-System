a2e6be93e7292919439810eb2be9a2e2dbebdfd7
ducnn <ducnn.230288@gmail.com>
2022-06-08 09:12:15 +0700

    feat(main): optimal 7/6/2022

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  plugins: ["react", "prettier", "import", "jsx-a11y", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "standard",
+    "prettier"
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "react/prop-types": 0,
+    "no-underscore-dangle": 0,
+    "camelcase": "off",
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+  },
+  "globals": {
+    "window": true,
+    "document": true,
+    "localStorage": true,
+    "FormData": true,
+    "FileReader": true,
+    "Blob": true,
+    "caches": true
+  }
+};
